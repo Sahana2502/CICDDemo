16191a5bf24fd8f654f90782ef1bf7630fef9acc
Sahana Venkatesh <svenkatesh@tvo.org>
2022-05-04 14:30:37 -0400

    nothing new

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <img
+        src="https://i.kym-cdn.com/entries/icons/original/000/018/929/graphic_design_is_my_passion.png"
+        alt=""
+      />{" "}
+      */
       <p>
         This is an app to demo AWS Codepipeline! I just have to push to github
         and Volia! everything is deployed
